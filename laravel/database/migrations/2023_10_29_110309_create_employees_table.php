851625a7bbeeae38e031340a83018b4f009e87bd
DevO <walidalyousef@gmail.com>
2023-10-29 16:53:16 +0300

    Commented with my corrections

--- laravel/database/migrations/2023_10_29_110309_create_employees_table.php
+++ laravel/database/migrations/2023_10_29_110309_create_employees_table.php
@@ -11,18 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // if (!Schema::hasTable('employees')) {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('family');
-            $table->string('father');
-            $table->string('mother');
-            $table->string('birth_place');
-            $table->date('birth_date');
+            $table->string('name'); // first_name
+            $table->string('family'); // last_name
+            $table->string('father'); // middle_name
+            $table->string('mother'); // mother_name
+            $table->string('birth_place'); 
+            $table->date('birth_date'); // birthday
             $table->enum('sex' , ['m' , 'f']);
             $table->enum('title' , ['Mr' , 'Ms' , 'Dr' ,'Esq']);
             $table->string('national_number');
-            $table->string('pass_port_number')->nullable();
+            $table->string('pass_port_number')->nullable(); // passport_number
             $table->string('address');
             $table->string('landline');
             $table->foreignId('department_id')->constrained('departments')->onDelete('cascade');
@@ -32,7 +33,9 @@ return new class extends Migration
             $table->string('children')->nullable();
             $table->string('exp_brief')->default('No previous experience');
             $table->timestamps();
+            // Add softdelete column
         });
+        // }
     }
 
     /**
