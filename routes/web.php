4649d5aaebc55010a954fb6f0e2ecf1798267fce
KaramFarha <karamfarha.dota@gmail.com>
2023-10-30 17:25:40 +0300

    fix path

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\DepartmentController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('employee.index');
 });
 
 Auth::routes(['register' => false]);
